1635beaae6f6c517b110c5595bee3f43d20b9cbf
fracasit0 <tomas.orellana@mail.udp.cl>
2021-06-04 02:17:57 -0400

    auth endpoints

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-import '@babel/polyfill';
+import "@babel/polyfill";
+var cors = require("cors");
 
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 //const path = require('path');
 //const cors = require('cors');
 //const cookieParser = require('cookie-parser');
@@ -13,32 +14,35 @@ const app = express();
 
 //settings
 
-app.set('port', process.env.PORT || 4000);
+app.set("port", process.env.PORT || 4000);
 
 //middlewares server
 
 //app.use(cookieParser());
 //app.use(cors());
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //variables globales
 
-app.use((req,res,next) => {
-    next();
+app.use((req, res, next) => {
+  next();
 });
 
 //Importar rutas
-
-import mision from './routes/mision.routes.js';
+import mision from "./routes/mision.routes.js";
+import shipment from "./routes/embarques.routes";
+import auth from "./routes/auth.routes";
 
 //routes
-app.use('/mision', mision);
+app.use(cors());
+app.use("/mision", mision);
+app.use("/shipment", shipment);
+app.use("/auth", auth);
 
 //public
 
 //app.use(express.static('../public'));
 
 export default app;
-

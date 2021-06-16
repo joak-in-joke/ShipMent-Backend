fab2ca617f15b02a9cfe3095b0a7dd940075cff3
joak-in-joke <joaquin.canzalez@gmail.com>
2021-06-15 20:44:47 -0400

    Controlador del Filtro

--- src/controllers/filtro.controller.js
+++ src/controllers/filtro.controller.js
@@ -1,54 +1,85 @@
 import embarques from "../models/embarques";
 import dataembarque from "../models/dataembarque";
-
+var Sequelize = require("sequelize");
 export async function filterEmbarque(req, res) {
   const { filtro, busqueda } = req.body;
-  var query;
-  switch (filtro) {
-    case "id":
+  if (busqueda && filtro) {
+    var query;
+    switch (filtro) {
+      case "id":
+        query = {
+          where: {
+            id: busqueda,
+          },
+        };
+        break;
+
+      case "status":
+        query = {
+          where: {
+            estado: busqueda,
+          },
+        };
+        break;
+
+      case "referencia":
+        query = {
+          where: {
+            referencia: busqueda,
+          },
+        };
+        break;
+
+      case "etd":
+        query = {
+          where: {
+            etd: busqueda,
+          },
+        };
+
+      case "eta":
+        query = {
+          where: {
+            eta: busqueda,
+          },
+        };
+        break;
+    }
+
+    try {
+      const embarque = await embarques.findAll(query);
+      res.json(embarque);
+    } catch (error) {
+      console.log(error);
+    }
+  } else if (busqueda) {
+    console.log("wenawena");
+
+    if (typeof busqueda == "number") {
       query = {
         where: {
-          id_embarque: busqueda,
+          [Sequelize.Op.or]: [{ id: busqueda }, { n_operacion: busqueda }],
         },
       };
-      break;
-
-    case "status":
+    } else if (typeof busqueda == "string") {
       query = {
         where: {
-          estado: busqueda,
+          [Sequelize.Op.or]: [
+            { estado: busqueda },
+            { referencia: busqueda },
+            { medio_transporte: busqueda },
+            { tipo_operacion: busqueda },
+          ],
         },
       };
-      break;
-
-    case "referencia":
-      query = {
-        where: {
-          referencia: busqueda,
-        },
-      };
-      break;
-
-    case "etd":
-      query = {
-        where: {
-          etd: busqueda,
-        },
-      };
-
-    case "eta":
-      query = {
-        where: {
-          eta: busqueda,
-        },
-      };
-      break;
-  }
-
-  try {
-    const embarque = await embarques.findOne(query);
-    res.json(embarque);
-  } catch (error) {
-    console.log(error);
+    }
+    try {
+      const embarque = await embarques.findAll(query);
+      res.json(embarque);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.json(await embarques.findAll());
   }
 }

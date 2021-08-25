0365177e5ad453e94bb709d3ed8ef879d453c245
joak-in-joke <joaquin.canzalez@gmail.com>
2021-08-25 18:41:16 -0400

    add shipment

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,7 +1,5 @@
 var signIn = require("./signIn");
-var verifyToken = require("./verifyToken");
 
 module.exports = {
   signIn,
-  verifyToken,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 // Import controllers
-var { signIn, verifyToken } = require("../controllers/auth");
+var { signIn } = require("../controllers/auth");
 
 // Routes
 router.post("/signin", signIn);
-router.post("/verifysesion", verifyToken);
 
 module.exports = router;

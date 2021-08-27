a24bb746287acc549ac516c7f7d4b11da4a2eff2
joak-in-joke <joaquin.canzalez@gmail.com>
2021-08-26 22:24:53 -0400

    create shipments fix

--- controllers/shipment/index.js
+++ controllers/shipment/index.js
@@ -1,7 +1,9 @@
 var getShipment = require("./getShipment");
 var deleteShipment = require("./deleteShipment");
+var createShipment = require("./createShipment");
 
 module.exports = {
   getShipment,
   deleteShipment,
+  createShipment,
 };
--- models/mision.js
+++ models/mision.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: "Misions",
+      modelName: "Mision",
       paranoid: true,
     }
   );

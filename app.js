a01ae87424123242e3acd1ff2a9b0c776b6851b7
joak-in-joke <joaquin.canzalez@gmail.com>
2021-08-30 22:53:30 -0400

    shipment edit

--- app.js
+++ app.js
@@ -7,17 +7,6 @@ var logger = require("morgan");
 const port = 4000;
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var shipmentsRouter = require("./routes/shipments");
-var shipmentRouter = require("./routes/shipment");
-var misionsRouter = require("./routes/misions");
-var authRouter = require("./routes/auth");
-var dashboardRouter = require("./routes/dashboard");
-var portsRouter = require("./routes/ports");
-var providerRouter = require("./routes/provider");
-var timelineRouter = require("./routes/timeline");
-var agentRouter = require("./routes/agent");
-var operatorRouter = require("./routes/operator");
 
 var app = express();
 
@@ -29,17 +18,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/shipments", shipmentsRouter);
-app.use("/shipment", shipmentRouter);
-app.use("/mision", misionsRouter);
-app.use("/auth", authRouter);
-app.use("/dashboard", dashboardRouter);
-app.use("/port", portsRouter);
-app.use("/provider", providerRouter);
-app.use("/timeline", timelineRouter);
-app.use("/agent", agentRouter);
-app.use("/operator", operatorRouter);
 
 app.listen(port, () => {
   console.log(`Shipment Backend initialized puerto: ${port}`);
--- controllers/shipment/index.js
+++ controllers/shipment/index.js
@@ -1,9 +1,11 @@
 var getShipment = require("./getShipment");
 var deleteShipment = require("./deleteShipment");
 var createShipment = require("./createShipment");
+var editShipment = require("./editShipment");
 
 module.exports = {
   getShipment,
   deleteShipment,
   createShipment,
+  editShipment,
 };
